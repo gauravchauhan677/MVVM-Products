2b27d96721c478ce9c64c0e7bbfc4609dfdc109b
Gaurav chauhan <gauravchauhan677@gmail.com>
2022-12-31 21:48:46 +0530

    generic api code added

---
+++ MVVM Products/Utility/EndPointType.swift
@@ -0,0 +1,48 @@
+//
+//  EndPointType.swift
+//  MVVM Products
+//
+//  Created by Gaurav Chauhan on 31/12/22.
+//
+
+import Foundation
+
+enum HTTPMethods: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+protocol EndPointType {
+    var path: String { get }
+    var baseURL: String { get }
+    var url: URL? { get }
+    var method: HTTPMethods { get }
+}
+
+enum EndPointItems {
+    case products
+}
+
+extension EndPointItems: EndPointType {
+    var path: String {
+        switch self {
+        case .products:
+            return "products"
+        }
+    }
+    
+    var baseURL: String {
+        return "https://fakestoreapi.com/"
+    }
+    
+    var url: URL? {
+        return URL(string: "\(baseURL)\(path)")
+    }
+    
+    var method: HTTPMethods {
+        switch self {
+        case .products:
+            return .get
+        }
+    }
+}
